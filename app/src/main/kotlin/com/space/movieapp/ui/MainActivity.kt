0a8892956a6ce314ebf466195352419d9f499944
davit.takadze <davit.takadze@space.ge>
2026-06-16 11:55:49 +0400

    refactor: add import in MainActivity file

--- app/src/main/kotlin/com/space/movieapp/ui/MainActivity.kt
+++ app/src/main/kotlin/com/space/movieapp/ui/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.space.movieapp.navigation.MovieNavigation
-import com.example.ui.theme.MovieAppTheme
+import com.space.ui.theme.MovieAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
