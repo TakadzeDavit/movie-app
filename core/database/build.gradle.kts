b9862733aa627d8fc60362f42ba376e1570c0c22
davit.takadze <davit.takadze@space.ge>
2026-06-30 17:28:25 +0400

    implement room database

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -1,30 +1,16 @@
 plugins {
-    alias(libs.plugins.android.library)
+    alias(libs.plugins.movie.android.library)
+    alias(libs.plugins.movie.android.koin)
+    alias(libs.plugins.devtools.ksp)
 }
 
 android {
     namespace = "com.space.core.database"
-    compileSdk {
-        version = release(37)
-    }
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
 }
 
 dependencies {
-    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(libs.androidx.junit)
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+
+    ksp(libs.room.compiler)
 }
---
+++ core/database/src/main/kotlin/com/space/core/database/di/DatabaseModule.kt
@@ -0,0 +1,19 @@
+package com.space.core.database.di
+
+import androidx.room.Room
+import com.space.core.database.AppDatabase
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+import kotlin.jvm.java
+
+val databaseModule = module {
+    single {
+        Room.databaseBuilder(
+            androidContext(),
+            AppDatabase::class.java,
+            "movie_app_database"
+        ).build()
+    }
+
+    single { get<AppDatabase>().genreDao() }
+}
