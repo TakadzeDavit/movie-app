f2805f677d266ef65739424fdabf8b044caa99d9
davit.takadze <davit.takadze@space.ge>
2026-07-06 17:57:28 +0400

    add placeholder to favorites screen

--- feature/favorites/presentation/src/main/kotlin/com/space/movieapp/feature/favorites/presentation/screen/FavoritesScreen.kt
+++ feature/favorites/presentation/src/main/kotlin/com/space/movieapp/feature/favorites/presentation/screen/FavoritesScreen.kt
@@ -1,21 +1,27 @@
 package com.space.movieapp.feature.favorites.presentation.screen
 
+import android.widget.Space
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -89,27 +95,49 @@ private fun FavoritesContent(
             }
 
             is DataState.Success -> {
-                LazyVerticalGrid(
-                    columns = GridCells.Fixed(2),
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(horizontal = 16.dp),
-                    horizontalArrangement = Arrangement.spacedBy(16.dp),
-                    verticalArrangement = Arrangement.spacedBy(16.dp)
-                ) {
-                    items(movieState.data, key = { it.id }) { movie ->
-                        MovieCatalogueCard(
-                            imgUrl = movie.posterPath ?: "",
-                            genre = movie.genre,
-                            title = movie.title,
-                            isFavorite = movie.isFavorite,
-                            year = movie.releaseDate,
-                            showFilterName = movie.genre,
-                            onFavoriteClick = {
-                                onEvent(FavoritesEvent.RemoveFromFavorites(movie.id))
-                            },
-                            onCardClick = { onMovieClick(movie.id) }
+                if (movieState.data.isEmpty()) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.icon_no_results),
+                            tint = Color.Unspecified,
+                            contentDescription = null
                         )
+
+                        Spacer(modifier = Modifier.height(Spacing.spacing26))
+
+                        Text(
+                            text = stringResource(R.string.no_movies_added_yet),
+                            color = colors.textSecondary,
+                            style = typography.titleMedium
+                        )
+                    }
+                } else {
+                    LazyVerticalGrid(
+                        columns = GridCells.Fixed(2),
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(horizontal = Spacing.spacing16),
+                        horizontalArrangement = Arrangement.spacedBy(Spacing.spacing16),
+                        verticalArrangement = Arrangement.spacedBy(Spacing.spacing16)
+                    ) {
+                        items(movieState.data, key = { it.id }) { movie ->
+                            MovieCatalogueCard(
+                                imgUrl = movie.posterPath ?: "",
+                                genre = movie.genre,
+                                title = movie.title,
+                                isFavorite = movie.isFavorite,
+                                year = movie.releaseDate,
+                                showFilterName = movie.genre,
+                                onFavoriteClick = {
+                                    onEvent(FavoritesEvent.RemoveFromFavorites(movie.id))
+                                },
+                                onCardClick = { onMovieClick(movie.id) }
+                            )
+                        }
                     }
                 }
             }
