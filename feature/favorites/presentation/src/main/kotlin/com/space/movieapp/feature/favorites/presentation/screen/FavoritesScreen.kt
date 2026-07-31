a20730bb0821b7e528010ba95c69121d2ce3c230
davit.takadze <davit.takadze@space.ge>
2026-07-31 10:25:28 +0400

    add favorite scope

--- feature/favorites/presentation/src/main/kotlin/com/space/movieapp/feature/favorites/presentation/screen/FavoritesScreen.kt
+++ feature/favorites/presentation/src/main/kotlin/com/space/movieapp/feature/favorites/presentation/screen/FavoritesScreen.kt
@@ -22,17 +22,20 @@ import com.space.movieapp.feature.favorites.presentation.component.EmptyFavorite
 import com.space.movieapp.feature.favorites.presentation.component.FavoritesHeader
 import com.space.movieapp.feature.favorites.presentation.contract.FavoritesEvent
 import com.space.movieapp.feature.favorites.presentation.contract.FavoritesState
+import com.space.movieapp.feature.favorites.presentation.di.FavoriteScope
 import com.space.movieapp.feature.favorites.presentation.vm.FavoritesVM
 import com.space.ui.component.card.MovieCatalogueCard
 import com.space.ui.component.error.ErrorScreen
 import com.space.ui.theme.MovieAppTheme
 import com.space.ui.theme.MovieTheme.colors
 import com.space.ui.theme.Spacing
+import org.koin.core.qualifier.named
 
 @Composable
 fun FavoritesScreen() {
     BaseScreen(
         vmClass = FavoritesVM::class,
+        scopeQualifier = named<FavoriteScope>(),
         content = { state, onEvent ->
             FavoritesContent(
                 state = state,
